903a58686c8a7c7d57a373fc605841f77856b9b4
Steven Cobb <sjcobb.dev@gmail.com>
2019-02-17 22:07:04 -0500

    three.js fire rendering from codepen volumetric

--- js/fire.js
+++ js/fire.js
@@ -308,12 +308,32 @@ function createEnvironment() {
     createRiverOutput();
     createRiver(riverOutput.startX * 2, 0, scr.w, waterfall.h + 2, wall.pZ);
 
-    createFire();
+    //createFire();
     createSkyBox();
 }
 
 function createFire() {
+    var loader = new THREE.TextureLoader();
+    loader.crossOrigin = '';
 
+    var fireTex = loader.load("https://s3-us-west-2.amazonaws.com/s.cdpn.io/212131/Fire.png");
+
+    var wireframeMat = new THREE.MeshBasicMaterial({
+        color : new THREE.Color(0xffffff),
+        wireframe : true
+    });
+
+    fire = new THREE.Fire(fireTex);
+
+    var wireframe = new THREE.Mesh(fire.geometry, wireframeMat.clone());
+    fire.add(wireframe);
+    wireframe.visible = true;
+    wireframe.visible = false;
+    
+    console.log({fire});
+    fire.position.set(0, 0, 0);
+    fire.position.set(0, 0.25, 1.3);
+    scene.add(fire);
 }
 
 /* Function that creates the grey box / waterfall wall.  */
@@ -628,6 +648,11 @@ function render() {
 
     // if (treeTrunk.visible == false) 
     //     treeTrunk.visible = true;
+
+
+    /*** FIRE ***/
+    //var t = clock.elapsedTime;
+    //fire.update(t);
 
     renderer.render(scene, camera);
 
